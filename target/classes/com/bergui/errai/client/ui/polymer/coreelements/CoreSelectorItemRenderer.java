fa995df8ccb3123d44bce3ceff51a7e352883963
GUIBERT Julien <guibertjulien@gmail.com>
2015-03-08 15:58:55 +0100

    clean

--- target/classes/com/bergui/errai/client/ui/polymer/coreelements/CoreSelectorItemRenderer.java
+++
@@ -1,9 +0,0 @@
-package com.bergui.errai.client.ui.polymer.coreelements;
-
-import com.google.gwt.dom.client.Element;
-
-public abstract class CoreSelectorItemRenderer<T> {
-	
-	public abstract Element render(T item);
-
-}
